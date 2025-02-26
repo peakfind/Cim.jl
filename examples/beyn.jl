7a42a4928657a4e76fd96cb6074a7366fc45673e
jyzhang <peakfind@126.com>
2025-02-26 20:42:12 +0100

    test

--- examples/beyn.jl
+++ examples/beyn.jl
@@ -30,16 +30,12 @@ l = 10
 # Define the contour
 elp = Cim.ellipse([150, 0], 148, 148)
 
-# Get the quadrature nodes
-pts = get_quadpts(elp, N)
-
 # Show the contour and quadrature points on the contour
 fig = Figure()
 ax = Axis(fig[1, 1], aspect = 1, title = "Contour")
 show_contour!(ax, elp)
-show_quadpts!(ax, pts)
 
 # Compute the eigenvalues and plot them on complex plane
-λ = contr_int(pts, nep, 400, l)
+λ = contr_int(elp, nep, 400, l; n=50)
 show_eigenvalues!(ax, λ)
 fig
