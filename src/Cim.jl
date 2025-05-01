d134b7480b8d1410dd807cc44ad9941975933c55
jyzhang <peakfind@126.com>
2025-05-01 17:58:37 +0200

    fix the bug due to file load order

--- src/Cim.jl
+++ src/Cim.jl
@@ -4,15 +4,15 @@ using LinearAlgebra
 using SparseArrays
 using CairoMakie
 
+include("nep.jl")
+export Qep
+
 include("contour.jl")
 export get_quadpts
 export show_contour!, show_quadpts!, show_eigenvalues!
 
 include("contour_int.jl")
 export cim, contr_int_ho
-
-include("nep.jl")
-export Qep
 
 include("utils.jl")
 
